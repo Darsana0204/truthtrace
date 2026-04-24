b19e1741d29a404d7cc2cc0bce831fc6cabfd39d
Darsana <darsanar1420@gmail.com>
2026-04-24 15:55:43 +0530

    Made changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -181,7 +181,7 @@ app.post("/upload", upload.single("media"), async (req, res) => {
 // ===================== START SERVER =====================
 
 // IMPORTANT for cloud deployment
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
